6c41d1432adfcec744eae3b11d85a7d4ac0b33dc
nirey <nirey@frba.edu.utn.ar>
2024-04-02 21:07:53 +0000

    .

---
+++ cpu/include/main.h
@@ -0,0 +1,13 @@
+#ifndef CPU_MAIN_H_
+#define CPU_MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <commons/txt.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <readline/readline.h>
+#include <commons/collections/list.h>
+#include <commons/config.h>
+
+#endif
