8b1bc93c1824517fab1d8ff151fa7edab0a68f09
nirey <nirey@frba.edu.utn.ar>
2024-04-07 01:20:33 +0000

    config listas kernel

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -7,7 +7,10 @@ int main() {
 
     APP_config config_valores = cargar_configuracion_kernel();
     // Imprime los valores utilizando printf y los especificadores de formato adecuados
-    printf("Puerto CPU Dispatch: %d\n", config_valores.puerto_cpu_dispatch); //linea solo para testear funcionamienito, dsp borrar
+    printf("Puerto CPU Dispatch: %d\n", config_valores.puerto_cpu_dispatch); //lineas solo para testear funcionamienito, dsp borrar
     
+    log_trace (kernel_log_debug, "Recursos: %s|%s|%s",config_valores.recursos[0], config_valores.recursos[1], config_valores.recursos[2]);
+    log_debug (kernel_log_debug, "Recursos: %s|%s|%s",config_valores.instancias_recursos  [0], config_valores.instancias_recursos  [1], config_valores.instancias_recursos  [2]);
+
     return 0;
 }
