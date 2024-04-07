9fc844404b605f09f432559086032de641e616cf
nirey <nirey@frba.edu.utn.ar>
2024-04-07 01:45:35 +0000

    dg

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -10,7 +10,7 @@ int main() {
     printf("Puerto CPU Dispatch: %d\n", config_valores.puerto_cpu_dispatch); //lineas solo para testear funcionamienito, dsp borrar
     
     log_trace (kernel_log_debug, "Recursos: %s|%s|%s",config_valores.recursos[0], config_valores.recursos[1], config_valores.recursos[2]);
-    log_debug (kernel_log_debug, "Recursos: %s|%s|%s",config_valores.instancias_recursos  [0], config_valores.instancias_recursos  [1], config_valores.instancias_recursos  [2]);
+    log_debug (kernel_log_debug, "Instancias Recursos: %s|%s|%s",config_valores.instancias_recursos  [0], config_valores.instancias_recursos  [1], config_valores.instancias_recursos  [2]);
 
     return 0;
 }
