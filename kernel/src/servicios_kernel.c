57a3fd6d7508c68f9c3a7a9dc345dd6b5178cd3e
ignacioRodriguezM <irodriguezmartin@frba.utn.edu.ar>
2024-04-10 21:42:38 +0000

    comento que hace asignar_pid

--- kernel/src/servicios_kernel.c
+++ kernel/src/servicios_kernel.c
@@ -1,6 +1,6 @@
 #include "../include/servicios_kernel.h"
 
-int asignar_pid (){
+int asignar_pid (){ //asigno un id al proceso que se crea, evita pid`s iguales si llegan 2 procesos
     int valor_pid;
     
     pthread_mutex_lock(&mutex_pid);
