c409469097b2041cd850a88ad4771a63545142e1
nirey <nirey@frba.edu.utn.ar>
2024-05-05 17:55:46 +0000

    .

--- kernel/src/colas_estados.c
+++ kernel/src/colas_estados.c
@@ -6,6 +6,9 @@ void inicializar_colas_sin_bloqueados()
     procesos_ready = queue_create();
     procesos_excec = queue_create();
     procesos_exit = queue_create();
+    if(strcmp(algoritmo_planificacion, "VRR")){
+    procesos_ready_con_prioridad = queue_create();
+    }
     
 }
 
@@ -16,4 +19,8 @@ void finalizar_colas_sin_bloqueados()
     queue_destroy(procesos_ready);
     queue_destroy(procesos_excec);
     queue_destroy(procesos_exit);
+
+    if(strcmp(algoritmo_planificacion, "VRR")){
+    queue_destroy(procesos_ready_con_prioridad);
+    }
 }
