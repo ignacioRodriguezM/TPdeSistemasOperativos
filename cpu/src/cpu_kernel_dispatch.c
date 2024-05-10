44a726afd13d79c86689f1220012a6f3ac12e4b2
nirey <nirey@frba.edu.utn.ar>
2024-05-10 02:40:53 +0000

    Co-autho

--- cpu/src/cpu_kernel_dispatch.c
+++ cpu/src/cpu_kernel_dispatch.c
@@ -95,29 +95,3 @@ void _desalojar_proceso_por_quantum()
 
     destruir_paquete(a_enviar);
 }
-
-void _desalojar_proceso_por_exit()
-{
-    t_buffer *buffer = crear_buffer();
-    //[pid] [pc] [quantum] [registros]
-
-    cargar_uint16_al_buffer(buffer, PID);
-    cargar_uint32_al_buffer(buffer, PC_registro);
-    cargar_int8_al_buffer(buffer, QUANTUM);
-    cargar_uint8_al_buffer(buffer, AX_registro);
-    cargar_uint8_al_buffer(buffer, BX_registro);
-    cargar_uint8_al_buffer(buffer, CX_registro);
-    cargar_uint8_al_buffer(buffer, DX_registro);
-    cargar_uint32_al_buffer(buffer, EAX_registro);
-    cargar_uint32_al_buffer(buffer, EBX_registro);
-    cargar_uint32_al_buffer(buffer, ECX_registro);
-    cargar_uint32_al_buffer(buffer, EDX_registro);
-    cargar_uint32_al_buffer(buffer, SI_registro);
-    cargar_uint32_al_buffer(buffer, DI_registro);
-
-    t_paquete *a_enviar = crear_paquete(DESALOJO_POR_EXIT, buffer);
-
-    enviar_paquete(a_enviar, fd_kernel_dispatch);
-
-    destruir_paquete(a_enviar);
-}
