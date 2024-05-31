2d6db8fbf54364a07b359ed3c407155de5c604c7
nirey <nirey@frba.edu.utn.ar>
2024-05-31 18:24:18 +0000

    .

--- cpu/src/diccionario.c
+++ cpu/src/diccionario.c
@@ -43,83 +43,65 @@ void inicializar_diccionarios() {
     dictionary_put(parametros, "DI", &DI_registro);
     dictionary_put(parametros, "PC", &PC_registro);
 
-    printf("Claves y valores del diccionario 'operation_switcher':\n");
-    for (int i = 0; i <= 18; i++) {
-        char *keys[] = {
-            "SET", "MOV_IN", "MOV_OUT", "SUM", "SUB", "JNZ",
-            "RESIZE", "COPY_STRING", "WAIT", "SIGNAL", "IO_GEN_SLEEP",
-            "IO_STDIN_READ", "IO_STDOUT_WRITE", "IO_FS_CREATE", "IO_FS_DELETE",
-            "IO_FS_TRUNCATE", "IO_FS_WRITE", "IO_FS_READ", "EXIT"
-        };
-        void* value = dictionary_get(operation_switcher, keys[i]);
-        printf("Clave: %s, Valor: %ld\n", keys[i], (uintptr_t)value);
-    }
 }
 
 void decode(char* funcion, int num_params, char** params) {
-    printf("Claves y valores del diccionario 'operation_switcher':\n");
-    for (int i = 0; i <= 18; i++) {
-        char *keys[] = {
-            "SET", "MOV_IN", "MOV_OUT", "SUM", "SUB", "JNZ",
-            "RESIZE", "COPY_STRING", "WAIT", "SIGNAL", "IO_GEN_SLEEP",
-            "IO_STDIN_READ", "IO_STDOUT_WRITE", "IO_FS_CREATE", "IO_FS_DELETE",
-            "IO_FS_TRUNCATE", "IO_FS_WRITE", "IO_FS_READ", "EXIT"
-        };
-        void* value = dictionary_get(operation_switcher, keys[i]);
-        printf("Clave: %s, Valor: %ld\n", keys[i], (uintptr_t)value);
-    }
     
     // Obtener la operación
     void* result = dictionary_get(operation_switcher, funcion);
+    /*
     if (result == NULL) {
         fprintf(stderr, "Error: Operación '%s' no encontrada.\n", funcion);
         return;
-    }
+    }*/
     int op_switcher = (int)(uintptr_t)result;
 
     // Obtener los parámetros
     void* void_params[num_params];
     for (int i = 0; i < num_params; ++i) {
         void_params[i] = dictionary_get(parametros, params[i]);
-        if (void_params[i] == NULL) {
-            fprintf(stderr, "Error: Parámetro '%s' no encontrado.\n", params[i]);
-            return;
-        }
     }
 
     // Ejecutar la operación correspondiente
     switch (op_switcher) {
         case 0: {
             // SET
+            log_info(cpu_logger, "PID: <%u> - Ejecutando: <%s> - <%s, %s>", PID, params[0], (params[1]));
             int valor0 = atoi(params[1]);
             SET(void_params[0], &valor0);
             break;
         }
         case 1: {
             // MOV_IN
+            log_info(cpu_logger, "PID: <%u> - Ejecutando: <%s> - <%s, %s>", PID, params[0], (params[1]));
             int direccion_fisica = atoi(params[1]);
             MOV_IN(params[0], &direccion_fisica);
             break;
         }
         case 2:
             // MOV_OUT
+            log_info(cpu_logger, "PID: <%u> - Ejecutando: <%s> - <%s, %s>", PID, params[0], (params[1]));
             break;
         case 3:
             // SUM
+            log_info(cpu_logger, "PID: <%u> - Ejecutando: <%s> - <%s, %s>", PID, params[0], (params[1]));
             SUM(void_params[0], void_params[1]);
             break;
         case 4:
             // SUB
+            log_info(cpu_logger, "PID: <%u> - Ejecutando: <%s> - <%s, %s>", PID, params[0], (params[1]));
             SUB(void_params[0], void_params[1]);
             break;
         case 5: {
             // JNZ
+            log_info(cpu_logger, "PID: <%u> - Ejecutando: <%s> - <%s, %s>", PID, params[0], (params[1]));
             int valor5 = atoi(params[1]);
             JNZ(void_params[0], &valor5);
             break;
         }
         case 6: {
             // RESIZE
+            log_info(cpu_logger, "PID: <%u> - Ejecutando: <%s> - <%s>", PID, (params[1]));
             int valor6 = atoi(params[0]);
             RESIZE(&valor6);
             break;
@@ -129,18 +111,21 @@ void decode(char* funcion, int num_params, char** params) {
             break;
         case 8: {
             // WAIT
+            log_info(cpu_logger, "PID: <%u> - Ejecutando: <%s> - <%s>", PID, params[0]);
             char* nombre_recurso8 = params[0];
             WAIT(nombre_recurso8);
             break;
         }
         case 9: {
             // SIGNAL
+            log_info(cpu_logger, "PID: <%u> - Ejecutando: <%s> - <%s>", PID, params[0]);
             char* nombre_recurso9 = params[0];
             SIGNAL(nombre_recurso9);
             break;
         }
         case 10: {
             // IO_GEN_SLEEP
+            log_info(cpu_logger, "PID: <%u> - Ejecutando: <%s> - <%s, %s>", PID, params[0], (params[1]));
             char* nombre_interfaz = params[0];
             int valor10 = atoi(params[1]);
             IO_GEN_SLEEP(nombre_interfaz, &valor10);
@@ -169,6 +154,7 @@ void decode(char* funcion, int num_params, char** params) {
             break;
         case 18:
             // EXIT
+            log_info(cpu_logger, "PID: <%u> - Ejecutando: <%s>");
             EXIT();
             break;
         default:
