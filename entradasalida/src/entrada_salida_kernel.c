a1eadf8b1cd928d8054bf2780cc0683f74470071
ignacioRodriguezM <irodriguezmartin@frba.utn.edu.ar>
2024-06-12 22:59:34 +0000

    io_stdin_read muy avanzada, almost done
    Co-authored-by: nicolasreypc <nicolasreypc@users.noreply.github.com>
    Co-authored-by: NicoReinoso <NicoReinoso@users.noreply.github.com>

--- entradasalida/src/entrada_salida_kernel.c
+++ entradasalida/src/entrada_salida_kernel.c
@@ -13,7 +13,7 @@ void atender_entrada_salida_kernel()
             // [nombre io][operacion]
             char *nombre_io_llegado_del_kernel = extraer_string_al_buffer(buffer_recibido);
             char *operacion_a_realizar = extraer_string_al_buffer(buffer_recibido);
-            
+
             if (strcmp(nombre_io_llegado_del_kernel, nombre) != 0)
             {
                 log_error(entrada_salida_log_debug, "ME LLEGO UNA OPERACION QUE NO DEBIA, YA QUE NO ESTA A MI NOMBRE");
@@ -26,8 +26,8 @@ void atender_entrada_salida_kernel()
                 uint16_t unidades_de_trabajo = extraer_uint8_al_buffer(buffer_recibido);
                 destruir_buffer(buffer_recibido);
                 log_info(entrada_salida_logger, "PID: %u - Operacion: %s", pid, operacion_a_realizar);
-                esperarMilisegundos (unidades_de_trabajo * tiempo_unidad_trabajo);
-                //DEVOLVER CONFIRMACION AL KERNEL
+                esperarMilisegundos(unidades_de_trabajo * tiempo_unidad_trabajo);
+                // DEVOLVER CONFIRMACION AL KERNEL
                 t_buffer *buffer_a_enviar = crear_buffer();
                 cargar_string_al_buffer(buffer_a_enviar, nombre); //[nombre][pid]
                 cargar_uint16_al_buffer(buffer_a_enviar, pid);
@@ -37,66 +37,28 @@ void atender_entrada_salida_kernel()
 
                 destruir_paquete(a_enviar);
             }
-            else if (strcmp(operacion_a_realizar, "IO_STDIN_READ") == 0){
-                uint16_t pid = extraer_uint16_al_buffer(buffer_recibido);
-                uint8_t  tamanio = extraer_uint8_al_buffer(buffer_recibido);
-                void* registro_direccion = extraer_choclo_al_buffer(buffer_recibido);
-
-                destruir_buffer(buffer_recibido);
+            else if (strcmp(operacion_a_realizar, "IO_STDIN_READ") == 0)
+            {
+                caso_io_stdin_read();
                 
-                log_info(entrada_salida_logger, "PID: %u - Operacion: %s", pid, operacion_a_realizar);
-//aca va lo que hay que hacer digamos
-            // primero debo enviar lo que se necesite a memoria
-                t_buffer *buffer_a_enviar_a_memoria = crear_buffer();
-
-                cargar_uint8_al_buffer(buffer_a_enviar_a_memoria, tamanio); //[nombre][pid]
-                cargar_choclo_al_buffer(buffer_a_enviar_a_memoria, registro_direccion);
-                
-                t_paquete *a_enviar_a_memoria = crear_paquete(IO_STDIN_READ, buffer_a_enviar_a_memoria);
-
-                enviar_paquete(a_enviar_a_memoria, fd_memoria);
-
-                destruir_paquete(a_enviar_a_memoria); 
-//espero a que memoria me de el ok para mandar confirmacion a kernel luego
-    int cod_op = recibir_operacion(fd_memoria);
-    switch (cod_op)
-    {
-    case RESPUESTA_MEMORIA:
-
-        t_buffer *recibido = recibir_buffer_sin_cod_op(fd_memoria);
-
-        char *mensaje_de_respuesta = extraer_string_al_buffer(recibido);
-        // log_info(cpu_log_debug, "RESPUESTA MEMORIA : %s", mensaje_de_respuesta);
-        destruir_buffer(recibido);
-                
-
-                //DEVOLVER CONFIRMACION AL KERNEL
-                t_buffer *buffer_a_enviar = crear_buffer();
-                cargar_string_al_buffer(buffer_a_enviar, nombre); //[nombre][pid]
-                cargar_uint16_al_buffer(buffer_a_enviar, pid);
-                t_paquete *a_enviar = crear_paquete(FIN_DE_EJECUCION_DE_IO, buffer_a_enviar);
-
-                enviar_paquete(a_enviar, fd_kernel);
-
-                destruir_paquete(a_enviar);                
             }
-            else if (strcmp(operacion_a_realizar, "IO_STDOUT_WRITE") == 0){
-
+            else if (strcmp(operacion_a_realizar, "IO_STDOUT_WRITE") == 0)
+            {
             }
-            else if (strcmp(operacion_a_realizar, "IO_FS_CREATE") == 0){
-
+            else if (strcmp(operacion_a_realizar, "IO_FS_CREATE") == 0)
+            {
             }
-            else if (strcmp(operacion_a_realizar, "IO_FS_DELETE") == 0){
-
+            else if (strcmp(operacion_a_realizar, "IO_FS_DELETE") == 0)
+            {
             }
-            else if (strcmp(operacion_a_realizar, "IO_FS_TRUNCATE") == 0){
-
+            else if (strcmp(operacion_a_realizar, "IO_FS_TRUNCATE") == 0)
+            {
             }
-            else if (strcmp(operacion_a_realizar, "IO_FS_WRITE") == 0){
-
+            else if (strcmp(operacion_a_realizar, "IO_FS_WRITE") == 0)
+            {
             }
-            else if (strcmp(operacion_a_realizar, "IO_FS_READ") == 0){
-
+            else if (strcmp(operacion_a_realizar, "IO_FS_READ") == 0)
+            {
             }
 
             break;
@@ -122,4 +84,57 @@ void presentarse_con_kernel()
     enviar_paquete(a_enviar, fd_kernel);
 
     destruir_paquete(a_enviar);
+}
+
+void caso_io_stdin_read()
+{
+    uint16_t pid = extraer_uint16_al_buffer(buffer_recibido);
+    uint8_t tamanio = extraer_uint8_al_buffer(buffer_recibido);
+    uint8_t cantidad_de_direcciones = extraer_uint8_al_buffer(buffer_recibido);
+
+
+    char* mensaje = obtener_mensaje_de_stdin();
+
+
+    for (int i = 0; i < cantidad_de_direccions; i++)
+    {
+        // cargar_uint8_al_buffer(pcb_a_editar->operacion_de_io_por_la_que_fue_bloqueado, tam_a_escribir_por_pagina);
+        // cargar_uint16_al_buffer(pcb_a_editar->operacion_de_io_por_la_que_fue_bloqueado, numero_de_pagina);
+        // cargar_uint32_al_buffer(pcb_a_editar->operacion_de_io_por_la_que_fue_bloqueado, desplazamiento);
+    }
+
+    destruir_buffer(buffer_recibido);
+
+    log_info(entrada_salida_logger, "PID: %u - Operacion: %s", pid, operacion_a_realizar);
+
+
+    t_buffer *buffer_a_enviar_a_memoria = crear_buffer();
+    
+    t_paquete *a_enviar_a_memoria = crear_paquete(ESCRITURA, buffer_a_enviar_a_memoria);
+
+    enviar_paquete(a_enviar_a_memoria, fd_memoria);
+
+    destruir_paquete(a_enviar_a_memoria);
+    // espero a que memoria me de el ok para mandar confirmacion a kernel luego
+    int cod_op = recibir_operacion(fd_memoria);
+    switch (cod_op)
+    {
+    case ESCRITURA:
+
+        t_buffer *recibido = recibir_buffer_sin_cod_op(fd_memoria);
+
+        char *mensaje_de_respuesta = extraer_string_al_buffer(recibido);
+        // log_info(cpu_log_debug, "RESPUESTA MEMORIA : %s", mensaje_de_respuesta);
+        destruir_buffer(recibido);
+
+        // DEVOLVER CONFIRMACION AL KERNEL
+        t_buffer *buffer_a_enviar = crear_buffer();
+        cargar_string_al_buffer(buffer_a_enviar, nombre); //[nombre][pid]
+        cargar_uint16_al_buffer(buffer_a_enviar, pid);
+        t_paquete *a_enviar = crear_paquete(FIN_DE_EJECUCION_DE_IO, buffer_a_enviar);
+
+        enviar_paquete(a_enviar, fd_kernel);
+
+        destruir_paquete(a_enviar);
+    }
 }
